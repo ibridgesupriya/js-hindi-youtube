ba7211e6dd9306c698dbf74ffc05052bf8651e6e
ibridgesupriya <139122781+ibridgesupriya@users.noreply.github.com>
2023-09-06 13:04:15 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"ues scrtict"; //treat all js code as newer version
+
+// alert (3 + 3) // we are using node js, not brower
+
+console.log(3 
+    +
+     3) // code readaibility should be high
+
+     console.log("supriya")
+
+
+     let name = "supriya"
+     let age = 22
+     let isLoggedIn = false
+     let state;
+
+
+     // nnumber => to power 53
+     // biginit
+     // boolean => true/false
+     //null  => standlone value
+     // uundefined =>
+     // symbol => unique
+
+     // object
+
+
+    console.log(typeof unudefined) // undefined
+    console.log(typeof null) // null
