0af4909b732153ec05bcc13f79a7767065cf3a4c
ibridgesupriya <139122781+ibridgesupriya@users.noreply.github.com>
2023-09-07 12:42:06 +0000

    datatypes summary

--- 01_basics.js/datatypes-summary.js
+++ 01_basics.js/datatypes-summary.js
@@ -37,3 +37,30 @@ console.log(typeof anotherId);
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
 
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+// stack (Primitive), heap (Non-primitive)
+
+let myofficename = "iBridge Digital"
+
+let anothername = "techsoft"
+anothername = "supriya"
+
+console.log();
+console.log(anothername);
+
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "supriya@12.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
