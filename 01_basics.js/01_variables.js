7a3e17780affa22e57293f527d4ab7ca1c1ea976
ibridgesupriya <139122781+ibridgesupriya@users.noreply.github.com>
2023-09-07 06:15:48 +0000

    conversion of datatypes

---
+++ 01_basics.js/01_variables.js
@@ -0,0 +1,31 @@
+const accountId = 12345
+let accountEmail = "suppy@sc.com"
+var accountPassword = "12345"
+accountCity = "Hyderabad"
+let accountState;
+
+
+// accountId = 2
+
+
+accountEmail = "suppy@.com"
+accountPassword = "23456"
+accountCity = "Hyderabad"
+
+
+console.log(accountId);
+
+/* 
+prefer not to use var
+because of issue in block cscope and functional scopee
+
+
+*/
+
+console.table([accountId,, accountEmail, accountPassword, accountCity, accountState])
+
+
+
+
+
+
