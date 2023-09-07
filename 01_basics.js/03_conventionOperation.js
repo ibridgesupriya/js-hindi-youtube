9caafe267873f89f047e21e467e96192a7640599
ibridgesupriya <139122781+ibridgesupriya@users.noreply.github.com>
2023-09-07 06:12:57 +0000

    Conversion of datatypes

---
+++ 01_basics.js/03_conventionOperation.js
@@ -0,0 +1,8 @@
+let score = "99"
+
+console.log(typeof score);
+console.log(typeod (score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
---
+++ 01_basics.js/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "99"
+
+//console.log(typeof score);
+//console.log(typeod (score));
+
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valuesNumber);
+
+// "99" => 99
+//"99abc" => NaN
+// true => 1; false => 0
+
+
+let iaLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "supriya" => true
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
