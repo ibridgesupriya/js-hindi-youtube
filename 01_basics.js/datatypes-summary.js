17e396ef09b9ac8a91990da1178d229dc593091a
ibridgesupriya <139122781+ibridgesupriya@users.noreply.github.com>
2023-09-07 12:14:51 +0000

    datatypes

---
+++ 01_basics.js/datatypes-summary.js
@@ -0,0 +1,39 @@
+// promitive
+
+// 7 types : string, number, boolean, null, undefined, symbol, biginit
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTem = null
+let userEmail;
+
+const id = ('')
+const anotherId = ('123')
+
+console.log(id === anotherId);
+const bigNumber = 233677788990654322n
+
+
+
+// reference (Non Primitive)
+
+//Array , objects, functions
+
+const heros = ["shakitman", "nagraj", "doga"];
+let myObj =  {
+    name: "supriya",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
