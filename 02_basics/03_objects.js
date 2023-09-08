1fedf0dc1c70eff28a531d962128bffe51696782
ibridgesupriya <139122781+ibridgesupriya@users.noreply.github.com>
2023-09-08 13:35:47 +0000

    objects in js

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,15 @@
+// singleton 
+
+// object.create
+// object literals
+const jsUser = {
+    name: "supriya",
+    age: "22",
+    location: "hyderabad",
+    email: "supriyachulika96@gmail.com",
+    isLoggedIn: false,
+    lastLoggedIn: ["Monday", "saturday"]
+}
+
+console.log(jaUser.email)
+console.log(jsUser[email])
