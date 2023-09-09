d985ff2b4b176117409c1138d3b604d5d8459999
ibridgesupriya <139122781+ibridgesupriya@users.noreply.github.com>
2023-09-09 09:32:29 +0000

    objects_part1

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -4,6 +4,7 @@
 // object literals
 const jsUser = {
     name: "supriya",
+    "full name": "Supriya",
     age: "22",
     location: "hyderabad",
     email: "supriyachulika96@gmail.com",
@@ -11,5 +12,38 @@ const jsUser = {
     lastLoggedIn: ["Monday", "saturday"]
 }
 
-console.log(jaUser.email)
-console.log(jsUser[email])
+// console.log(jsUser.email)
+// console.log(jsUser[email])
+// console.log(jsUser["full name"]) 
+// console.log(jsUser.mySym)
+
+jsUser.email = "supriyachiluka96@.com"
+Object.freeze(jsUser)
+jsUser.email = "supriyachiluka96@.com"
+console.log(jsUser); 
+
+
+jsUser.greeting = function(){
+    console.log("Hello js user")
+}
+
+jsUser.greetingTwo = function(){
+    console.log(`Helllo js user, ${This.name}`);
+}
+
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
